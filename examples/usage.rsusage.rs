dd2b80055989dc36b891acca855eca2e451f1e94
Manas Negi <manasnegi2006@gmail.com>
2025-06-26 18:20:52 +0530

    Delete examples/usage.rsusage.rs

--- examples/usage.rsusage.rs
+++
@@ -1,8 +0,0 @@
-use rust_crypto_wallet::*;
-
-fn main() {
-    let (secret, public) = generate_keypair();
-    let address = public_key_to_address(&public);
-
-    println!("Address: {}", address);
-}
